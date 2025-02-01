d0a7afdf22595228eeb02367d06e9a94480d0125
TMiyazono <miata3830@icloud.com>
2025-02-01 17:28:13 +0900

    20250201

---
+++ src/app/page.tsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+
+import { getAllTodos } from "../api";
+import AddTask from "./components/AddTask";
+import ToDoList from "./components/TodoList";
+
+export default async function Home() {
+  const tasks = await getAllTodos();
+  console.log(tasks);
+
+  return (
+    <main className="flex flex-col items-center justify-center min-h-screen py-2 bg-gray-200">
+      <h1 className="text-4xl font-bold text-gray-700 -mt-32">
+        Next 13 ToDo App
+      </h1>
+      <div className="w-full max-w-xl items-center justify-center mt-5">
+        <div className="w-full px-8 py-6 bg-white shadow-md rounded-lg">
+          <AddTask />
+          <ToDoList tasks={tasks} />
+        </div>
+      </div>
+    </main>
+  );
+}
